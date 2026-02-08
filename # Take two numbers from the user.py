8f790eebc5a84c0756fc4f3e6392e9d72a31e0c2
manandetroja1 <manandetroja6@gmail.com>
2026-02-08 23:13:42 +0530

    assigment 1
    i make 2 codes as per task

---
+++ # Take two numbers from the user.py
@@ -0,0 +1,20 @@
+# Take two numbers from the user
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+# Perform operations
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+
+# Handle division safely
+if num2 != 0:
+    division = num1 / num2
+else:
+    division = "Undefined (cannot divide by zero)"
+
+# Display results
+print("\nAddition:", addition)
+print("Subtraction:", subtraction)
+print("Multiplication:", multiplication)
+print("Division:", division)
